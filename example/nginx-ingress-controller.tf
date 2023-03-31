ef2b5b546b838e4b3a684f2d234cdaeaec7cde60
Anurag Singh <anuragsingh.in@gmail.com>
2023-03-31 14:19:36 +0800

    TLS termination at load balancer

--- example/nginx-ingress-controller.tf
+++ example/nginx-ingress-controller.tf
@@ -6,8 +6,9 @@ module "nginx_ingress_controller" {
   name          = var.nginx_ingress_chart_name
   chart_version = var.nginx_ingress_chart_version
 
-  controller_service_enableHttp = false
-  
+  controller_service_enableHttp        = false
+  controller_service_targetPorts_https = "http"
+
   aws_load_balancer_type      = "nlb"
   aws_load_balancer_ssl_cert  = var.alb_ssl_certificate_arn
 }
